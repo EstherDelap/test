a65b96dc16e2cc8d0ddfd2f967c7c84a7777d8b3
EstherDelap <py20ed@leeds.ac.uk>
2023-07-25 15:17:03 +0100

    variables

--- main.jl
+++ main.jl
@@ -3,13 +3,13 @@ using JLD2
 
 
 
-p1,p2,p3,p4 = split(read(ARGS[1], String), r"\s+")
+#p1,p2,p3,p4 = split(read(ARGS[1], String), r"\s+")
 
-function main(p1, p2, p3, p4)
+function main(p1, p2, p3)
 
     outfile = "results.jld2"
 
-    jldsave(outfile; collective_spin =[p1,p2,p3,p4])
+    jldsave(outfile; collective_spin =[p1,p2,p3])
 end
 
-main(p1, p2, p3, p4)
+main(ARGS[1],ARGS[2],ARGS[3])
--- submit.sh
+++ submit.sh
@@ -28,20 +28,23 @@
 #$ -N OTHER
 
 # Parse parameter file to get variables.
-#number=$SGE_TASK_ID
-#paramfile=/home/ucanede/Scratch/test/params.txt
+number=$SGE_TASK_ID
+paramfile=/home/ucanede/Scratch/test/input/params.txt
 
 
 BASE=$PWD
-LOAD=$BASE/input
+#LOAD=$BASE/input
 SAVE=$BASE/results
 
 module load julia/1.9.1
 
-number=$SGE_TASK_ID
+index="`sed -n ${number}p $paramfile | awk '{print $1}'`"
+variable1="`sed -n ${number}p $paramfile | awk '{print $2}'`"
+variable2="`sed -n ${number}p $paramfile | awk '{print $3}'`"
+variable3="`sed -n ${number}p $paramfile | awk '{print $4}'`"
 
 cd $TMPDIR
 cp $BASE/main.jl $TMPDIR/
-julia --project=$BASE main.jl $LOAD/params.txt
+julia --project=$BASE main.jl variable1 variable2 variable3 
 #julia --project=$BASE main.jl [1,2,3,4]
 mv results.jld2 $SAVE/results.jld2
